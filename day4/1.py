52742e9d1db4fa58def0c66400ca73979a2d08ed
Drew Barrett <dfb1998@gmail.com>
2015-12-04 17:34:13 -0500

    day 4 finished

---
+++ day4/1.py
@@ -0,0 +1,11 @@
+import hashlib
+md5hash = hashlib.md5()
+secret = "ckczppom"
+number = 0
+#md5hash.update("abcdef609043")
+print(md5hash.hexdigest())
+while md5hash.hexdigest()[0:5] != "00000":
+    number += 1
+    md5hash = hashlib.md5(secret + str(number))
+    print(md5hash.hexdigest() + " " + str(number) + " " + secret + str(number))
+print(str(number) + " hex: " + md5hash.hexdigest())

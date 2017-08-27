f363ab9d28d16bd80a2af5c13bf30baa9a99ef45
Drew Barrett <dfb1998@gmail.com>
2017-08-27 15:01:30 -0400

    progress

--- day5/2.py
+++ day5/2.py
@@ -1,28 +1,20 @@
-txt = open("input.txt")
-txtinput = txt.read()
-count = 0
-start = -1
-for i, c in enumerate(txtinput):
-    if c != "\n" and start == -1:
-        start = i
-    if c == "\n":
-        line = txtinput[start:i]
-        print(line)
-        firstletter = "0"
-        secondletter = "0"
-        test1 = False
-        for letter in line:
-            if firstletter == "0":
-                firstletter = letter
-            elif secondletter == "0":
-                secondletter = letter
-            elif firstletter == letter:
-                test1 = True
-                print("test 1 passed" + firstletter + secondletter + letter)
-                firstletter = "0"
-                secondletter = "0"
-                break
-            firstletter = secondletter
-            secondletter = letter
+# Pair of 2 letters that appears twice without overlapping
+# xyxy: yes
+# aaa: no
+def test1(string):
+    nextCharPos = 1
+    for char in string:
+        if nextCharPos < len(string):
+            pair = char + string[nextCharPos]
+            print (pair)
+        nextCharPos += 1
 
-print(str(count))
+def main():
+    with open("input.txt") as f:
+        content = f.read()
+    for line in content.splitlines():
+        print(line)
+        test1(line)
+    return
+
+main()
